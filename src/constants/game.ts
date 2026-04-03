38738e8c7bdb122dc93372086fdbcbe48cca6814
Fethi <89661953+fethiaksit@users.noreply.github.com>
2026-04-03 15:38:18 +0300

    Add consistent vertical drag offset for piece ghost and placement

--- src/constants/game.ts
+++ src/constants/game.ts
@@ -1,6 +1,7 @@
 export const BOARD_SIZE = 8;
 export const HAND_SIZE = 3;
 export const DRAG_SENSITIVITY = 1.18;
+export const DRAG_OFFSET_Y = -60;
 
 export const SCORE_VALUES = {
   lineClear: 10,
